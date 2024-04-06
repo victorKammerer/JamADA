522f77e1627164c641bf67b56cf44082f4f6e049
Victor Kammerer <victorkammerer@MacBook-Pro-de-Victor.local>
2024-04-06 00:50:19 -0300

    first commit

---
+++ JamADA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JamADA
+//
+//  Created by Victor Kammerer on 06/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JamADA/JamADAApp.swift
@@ -0,0 +1,17 @@
+//
+//  JamADAApp.swift
+//  JamADA
+//
+//  Created by Victor Kammerer on 06/04/24.
+//
+
+import SwiftUI
+
+@main
+struct JamADAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
