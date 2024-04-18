1b40afdcb8cd975f62c29c9aa94abe7757ffe929
Lucas Daniel Costa Da Silva <lucass.dcss75@gmail.com>
2024-04-18 20:15:49 -0300

    <fix> fix timer view to navigate automatically

--- JamADA/Core/TimerView.swift
+++ JamADA/Core/TimerView.swift
@@ -8,65 +8,56 @@
 import SwiftUI
 
 struct TimerView: View {
-    @StateObject private var viewModel: TimerViewModel
-    
-    let players: [Player]
-    @State var nextView: Bool = false
-    let countFrom: Int
-    
-    init(players: [Player], countFrom: Int) {
-        self.players = players
-        self._viewModel = StateObject(wrappedValue: TimerViewModel(countFrom: countFrom, players: players))
-        self.countFrom = countFrom
+  @StateObject private var viewModel: TimerViewModel
+  
+  let players: [Player]
+  let countFrom: Int
+  
+  init(players: [Player], countFrom: Int) {
+    self.players = players
+    self._viewModel = StateObject(wrappedValue: TimerViewModel(countFrom: countFrom, players: players))
+    self.countFrom = countFrom
+  }
+  
+  var body: some View {
+    VStack {
+      Spacer()
+      
+      if viewModel.isFirstTime {
+        Text("Cada um no seu lugar!") // Display placeholder text during the first 15 seconds
+          .font(.system(size:24, weight: .semibold, design: . rounded))
+      } else if viewModel.displayPlayerName {
+        Text("\(viewModel.currentPlayerName)") // Display player's name during 5-second intervals
+          .font(.headline)
+      }
+      
+      Text(viewModel.formattedTime())
+        .font(.system(size: 96, weight: .heavy, design: .rounded))
+      
+      RectangleButtonView(
+        buttonText: viewModel.timerRunning ? "Stop" : "Start",
+        textColor: nil,
+        buttonColor: nil,
+        action: {
+          viewModel.timerRunning.toggle()
+          if viewModel.timerRunning {
+            viewModel.resetTimer()
+            viewModel.startTimer()
+          }  else {
+            viewModel.stopTimer()
+          }
+        },
+        usesSymbol: false
+      )
+      Spacer()
+      NavigationLink(destination: GameVoteView(playerTurnVote: 0, selectedPlayer: nil).navigationBarBackButtonHidden(), isActive: $viewModel.lastPlayer){
+        EmptyView()
+      }.hidden()
     }
-    
-    var body: some View {
-        VStack {
-            Spacer()
-            
-            if viewModel.isFirstTime {
-                Text("Cada um no seu lugar!") // Display placeholder text during the first 15 seconds
-                    .font(.system(size:24, weight: .semibold, design: . rounded))
-            } else if viewModel.lastPlayer{
-        
-            } else if viewModel.displayPlayerName {
-                Text("\(viewModel.currentPlayerName)") // Display player's name during 5-second intervals
-                    .font(.headline)
-            }
-            
-            Text(viewModel.formattedTime())
-                .font(.system(size: 96, weight: .heavy, design: .rounded))
-            
-            NavigationLink(destination: GameVoteView(playerTurnVote: 0, votedPlayer: [Bool](repeating: false, count: players.count), votedPlayers: []).navigationBarBackButtonHidden(),
-                                   isActive: $nextView) {
-                        EmptyView()
-                    }
-                    .hidden()
-            
-            RectangleButtonView(
-                buttonText: viewModel.timerRunning ? "Stop" : "Start",
-                textColor: nil,
-                buttonColor: nil,
-                action: {
-                    viewModel.timerRunning.toggle()
-                    if viewModel.lastPlayer {
-                        nextView = true
-                    } else if viewModel.timerRunning {
-                        viewModel.resetTimer()
-                        viewModel.startTimer()
-                    }  else {
-                        viewModel.stopTimer()
-                    }
-                },
-                usesSymbol: false
-            )
-            
-            Spacer()
-        }
-    }
+  }
 }
 
 
 #Preview {
-    TimerView(players: [], countFrom: 5)
+  TimerView(players: [], countFrom: 5)
 }
